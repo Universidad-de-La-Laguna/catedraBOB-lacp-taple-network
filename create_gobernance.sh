19a97d770eebafcff61d88f1549d7093cce57c51
Benito Cuesta <benito.cuesta@ull.es>
2023-02-06 20:25:33 +0000

    feat: mejorar salida de script de creacion de gobernanza

--- create_gobernance.sh
+++ create_gobernance.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Comprobar prerequisitos
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    exit
+fi
+
 # Sustitucion de variables
 export MASTER_ID=`cat .credentials.master | sed -n 's/^MASTER_ID: \(.*\)$/\1/p'`
 export SLAVE1_ID=`cat .credentials.slave1 | sed -n 's/^SLAVE_ID: \(.*\)$/\1/p'`
@@ -7,7 +14,11 @@ export SLAVE2_ID=`cat .credentials.slave2 | sed -n 's/^SLAVE_ID: \(.*\)$/\1/p'`
 envsubst < lacp-gobernance.json.tmpl > temp-lacp-gobernance.json
 
 # Creacion de la gobernanza
-curl --location --request POST 'http://localhost:3000/api/requests' \
+CREATE_GOBERNANCE_RESULT=`curl -s --location --request POST 'http://localhost:3000/api/requests' \
 --header 'X-API-KEY;' \
 --header 'Content-Type: application/json' \
---data @temp-lacp-gobernance.json
+--data @temp-lacp-gobernance.json`
+
+GOBERNANCE_SUBJECT_ID=`jq -r .subject_id <<< $CREATE_GOBERNANCE_RESULT`
+
+echo "Gobernanza creada correctamente con subject_id: $GOBERNANCE_SUBJECT_ID"
